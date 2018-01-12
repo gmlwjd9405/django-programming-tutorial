86b60058746ff82ac27da5536ec250014cadfad7
kwonheejeong <gmlwjd9405@naver.com>
2018-01-12 13:13:31 +0900

    Implement basic Bookmark app

--- django-programming/mysite/urls.py
+++ django-programming/mysite/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from bookmark.views import BookmarkLV, BookmarkDV
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+
+    # class-based views for Bookmark app
+    url(r'^bookmark/$', BookmarkLV.as_view(), name='index'),
+    url(r'^bookmark/(?P<pk>\d+)/$', BookmarkDV.as_view(), name='detail')
 ]
